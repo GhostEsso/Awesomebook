b80a8d122cfa3532707c01e2b73f77659ce75c30
GhostEsso <lilipitaham@gmail.com>
2023-07-05 15:06:32 +0000

    Modification added

--- app.js
+++ app.js
@@ -1,3 +1,76 @@
+// Sélection des liens de la barre de navigation
+const booksLink = document.getElementById('booksLink');
+const addBookLink = document.getElementById('addBookLink');
+const contactLink = document.getElementById('contactLink');
+
+// Sélection des sections de contenu
+const bookListSection = document.getElementById('bookListSection');
+const addBookSection = document.getElementById('addBookSection');
+const contactSection = document.getElementById('contactSection');
+
+document.addEventListener('DOMContentLoaded', function() {
+  const currentDateElement = document.getElementById('current_date');
+  
+  function updateDateTime() {
+    const current_date = new Date();
+    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    const timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    const date = current_date.toLocaleDateString(undefined, dateOptions);
+    const time = current_date.toLocaleTimeString(undefined, timeOptions);
+    const dateTimeString = `${date} ${time}`;
+    
+    currentDateElement.textContent = dateTimeString;
+  }
+  
+  // Mettre à jour la date et l'heure chaque seconde
+  setInterval(updateDateTime, 1000);
+  
+  // Appel initial pour afficher la date et l'heure
+  updateDateTime();
+});
+
+//
+const logoLink = document.querySelector('.logo');
+
+logoLink.addEventListener('click', () => {
+  window.location.href = 'index.html';
+});
+
+
+
+
+// Écouteurs d'événements pour les liens de la barre de navigation
+booksLink.addEventListener('click', () => {
+  booksLink.classList.add('active');
+  addBookLink.classList.remove('active');
+  contactLink.classList.remove('active');
+
+  bookListSection.style.display = 'block';
+  addBookSection.style.display = 'none';
+  contactSection.style.display = 'none';
+});
+
+addBookLink.addEventListener('click', () => {
+  booksLink.classList.remove('active');
+  addBookLink.classList.add('active');
+  contactLink.classList.remove('active');
+
+  bookListSection.style.display = 'none';
+  addBookSection.style.display = 'block';
+  contactSection.style.display = 'none';
+});
+
+contactLink.addEventListener('click', () => {
+  booksLink.classList.remove('active');
+  addBookLink.classList.remove('active');
+  contactLink.classList.add('active');
+
+  bookListSection.style.display = 'none';
+  addBookSection.style.display = 'none';
+  contactSection.style.display = 'grid';
+
+});
+
 // Get reference to HTML elements
 const bookListElement = document.getElementById('bookList');
 const addBookForm = document.getElementById('addBookForm');
@@ -59,10 +132,17 @@ class BookCollection {
       } else {
         bookElement.classList.add('odd-book');
       }
-  });
+    });
 
     // Save the collection in localStorage
     localStorage.setItem('bookCollection', JSON.stringify(this.books));
+
+    // Update bookList border based on collection length
+    if (this.books.length === 0) {
+      bookListElement.classList.remove('border');
+    } else {
+      bookListElement.classList.add('border');
+    }
   }
 }
 
